ed83fbf88ba6ab6bd6d632d02329d0bd663fad33
mstepanova-xpirit <mstepanova@xpirit.com>
2024-07-30 12:45:04 +0200

    add 2 packages

---
+++ custom2/func.go
@@ -0,0 +1,11 @@
+// Checks if a number is prime or not
+package custom2
+
+func IsPrime2(num int) bool {
+	for i := 2; i < int(num/2); i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
